e4c7f21f4eb475481db0ee02cf36ec22f22b0fe0
iranroman <iran@ccrma.stanford.edu>
2023-05-10 15:37:35 -0400

    files and instructions to replicate SELD baseline results

---
+++ README.md
@@ -0,0 +1,56 @@
+# DCASE2022-data-generator
+Data generator for creating synthetic audio mixtures suitable for DCASE Challenge 2022 Task 3
+
+### Prerequisites
+
+The provided code was tested with Python 3.8 and the following libraries:
+SoundFile 0.10.3, mat73 0.58, numpy 1.20.1, scipy 1.6.2, librosa 0.8.1. 
+
+Must also download these RIR databases:
+* TAU SRIR DB: https://zenodo.org/record/6408611
+
+Also download
+* FSD50K https://zenodo.org/record/4060432
+* orchset (could be done using `mirdata`)
+
+```
+pip install mirdata
+```
+
+```
+>>> import mirdata
+>>> orchset = mirdata.initialize('orchset')
+>>> orchset = mirdata.initialize('orchset', data_home='/home/iran/datasets/orchset')     >>> orchset.download()
+```
+
+```
+cp /path/to/orchset/audio/mono/* /path/to/FSD50K/
+```
+
+## Getting Started
+
+This repository contains several Python file, which in total create a complete data generation framework.
+* The `generation_parameters.py` is a separate script used for setting the parameters for the data generation process, including things such as audio dataset, number of folds, mixuture length, etc.
+* The `db_config.py` is a class for containing audio filelists and data parameters from different audio datasets used for the mixture generation.
+* The `metadata_synthesizer.py` is a class for generating the mixture target labels, along with the corresponding metadata and statistics. Information from this class can be further used for synthesizing the final audios.
+* The `audio_synthesizer.py` is a class for synthesizing noiseless audio files containing the simulated mixtures.
+* The `audio_mixer.py` is a class for mixing the generated audio mixtures with background noise and/or interference mixtures.
+* The `make_dataset.py` is the main script in which the whole framework is used to perform the full data generation process.
+* The `utils.py` is an additional file containing complementary functions for other scripts.
+
+Moreover, two object files are included in case the database configuration via `db_config.py` takes too much time:
+* The `db_config_fsd.obj` is a DBConfig class containing information about the database and files for the FSD50K audioset.
+* The `db_config_nigens.obj` is a DBConfig class containing information about the database and files for the NIGENS audioset.
+
+The exemplary script is:
+* The `example_script_DCASE2022.py` is a script showing a pipeline to generate data similar to the current DCASE2022 dataset.
+
+The repository is licensed under the [TAU License](LICENSE.md).
+
+### Using the generated dataset to train the DCASE 2023 Task 3 audio-only baseline you should get results similar to these:
+
+
+| Dataset | ER<sub>20°</sub> | F<sub>20°</sub> | LE<sub>CD</sub> | LR<sub>CD</sub> |
+| ----| --- | --- | --- | --- |
+| Ambisonic (FOA + Multi-ACCDOA) | 0.00 | 00.0 % | 00.0&deg; | 00.0 % |
+| Microphone Array (MIC-GCC + Multi-ACCDOA) | 0.64 | 26.9 % | 23.8&deg; | 46.2 % |
---
+++ example_script_DCASE2022.py
@@ -0,0 +1,91 @@
+import sys
+import os
+import numpy as np
+from db_config import DBConfig
+from metadata_synthesizer import MetadataSynthesizer
+from audio_synthesizer import AudioSynthesizer
+from audio_mixer import AudioMixer
+import pickle
+from generation_parameters import get_params
+
+
+##############
+############## THIS IS AN EXEMPLARY SCRIPT GENERATING DATA
+############## SIMILAR TO THE DCASE2022 dataset
+##############
+
+
+# use parameter set defined by user
+task_id = '2' 
+
+params = get_params(task_id)
+    
+    ### Create database config based on params (e.g. filelist name etc.)
+#db_config = DBConfig(params)
+    
+    # LOAD DB-config which is already done
+db_handler = open('db_config_fsd.obj','rb')
+db_config = pickle.load(db_handler)
+db_handler.close()
+
+# fix the music files (assuming orchset)
+all_music_files = [f for f in os.listdir(params['db_path']) if 'ex' in f]
+tr_music = [f for f in all_music_files if 'Beethoven' not in f]
+ts_music = [f for f in all_music_files if 'Beethoven' in f]
+
+tr_samplelist = db_config._samplelist[0]
+i = 0
+sample_list = []
+for sample in tr_samplelist['audiofile']:
+    if 'music/' in sample:
+        sample = '/{}'.format(tr_music[i%len(tr_music)])
+        i += 1
+    sample_list.append(sample)
+db_config._samplelist[0]['audiofile'] = np.array(sample_list)
+
+ts_samplelist = db_config._samplelist[1]
+i = 0
+sample_list = []
+for sample in ts_samplelist['audiofile']:
+    if 'music/' in sample:
+        sample = '/{}'.format(ts_music[i%len(ts_music)])
+        i += 1
+    sample_list.append(sample)
+db_config._samplelist[1]['audiofile'] = np.array(sample_list)
+
+
+
+
+#create mixture synthesizer class
+noiselessSynth = MetadataSynthesizer(db_config, params, 'target_noiseless')
+    
+#create mixture targets
+mixtures_target, mixture_setup_target, foldlist_target = noiselessSynth.create_mixtures()
+    
+#calculate statistics and create metadata structure
+metadata, stats = noiselessSynth.prepare_metadata_and_stats()
+    
+#write metadata to text files
+noiselessSynth.write_metadata()
+    
+
+if not params['audio_format'] == 'both': # create a dataset of only one data format (FOA or MIC)
+    #create audio synthesis class and synthesize audio files for given mixtures
+    noiselessAudioSynth = AudioSynthesizer(params, mixtures_target, mixture_setup_target, db_config, params['audio_format'])
+    noiselessAudioSynth.synthesize_mixtures()
+        
+    #mix the created audio mixtures with background noise
+    audioMixer = AudioMixer(params, db_config, mixtures_target, mixture_setup_target, params['audio_format'], 'target_noisy')
+    audioMixer.mix_audio()
+else:
+    #create audio synthesis class and synthesize audio files for given mixtures
+    noiselessAudioSynth = AudioSynthesizer(params, mixtures_target, mixture_setup_target, db_config, 'foa')
+    noiselessAudioSynth.synthesize_mixtures()
+    noiselessAudioSynth2 = AudioSynthesizer(params, mixtures_target, mixture_setup_target, db_config, 'mic')
+    noiselessAudioSynth2.synthesize_mixtures()
+        
+    #mix the created audio mixtures with background noise
+    audioMixer = AudioMixer(params, db_config, mixtures_target, mixture_setup_target, 'foa', 'target_noisy')
+    audioMixer.mix_audio()
+    audioMixer2 = AudioMixer(params, db_config, mixtures_target, mixture_setup_target, 'mic', 'target_noisy')
+    audioMixer2.mix_audio()
