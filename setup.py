f806d5a8ec8a4c11d4214b9f13bcf812b18f57bd
Sivan <dswsivan@gmail.com>
2024-01-22 14:30:02 -0500

    update setup.py for installation

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ import imp
 with open('README.md') as file:
     long_description = file.read()
 
-version = imp.load_source('room_scaper.version', 'room_scaper/version.py')
+version = imp.load_source('spatialscaper.version', 'spatialscaper/version.py')
 
 setup(
-    name='room_scaper',
+    name='spatialscaper',
     version=version.version,
     description='A library for soundscape synthesis and augmentation using spatial impulse responses inside specific rooms',
     author='Iran R. Roman',
     author_email='iran@ccrma.stanford.edu',
     url='',
     download_url='http://github.com/iranroman/SELD-data-generator/releases',
-    packages=['room_scaper'],
+    packages=['spatialscaper'],
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords='audio sound soundscape environmental ambisonics microphone array sound event detection localization',
@@ -31,13 +31,15 @@ setup(
             "Topic :: Multimedia :: Sound/Audio :: Sound Synthesis",
         ],
     install_requires=[
-        'numpy>=1.24.4',
-        'scipy>=1.11.1',
-        'librosa>=0.10.0',
+        'numpy>=1.20',
+        'scipy>=1.10.1',
+        'librosa>=0.9.2',
         'mat73>=0.60',
         'fvcore>=0.1.5',
         'pysofaconventions',
         'pyroomacoustics',
+        'pandas',
+        'soundata',
     ],
 
 )
