77f8e617e5fb4e99e6c81fe431a4eb28a4cb5d59
iranroman <iran@ccrma.stanford.edu>
2024-01-23 20:26:52 -0500

    merging script utils

--- scripts/prepare_utils.py
+++ scripts/prepare_utils.py
@@ -1,3 +1,4 @@
+<<<<<<< scripts/prepare_utils.py
 import os
 import requests
 import tarfile
@@ -31,3 +32,46 @@ def download_file(url, local_dest_path):
             file.write(data)
 
     progress_bar.close()
+=======
+import argparse
+import sys
+import numpy as np
+
+def parse_args():
+    """
+    parse paths to specific files
+    """
+    parser = argparse.ArgumentParser(
+        description="Provide a path."
+    )
+    parser.add_argument(
+        "--path",
+        dest="path",
+        type=str,
+        help="path to relevant files",
+    )
+    if len(sys.argv) == 1:
+        parser.print_help()
+    return parser.parse_args()
+
+def map_to_cylinder(path, rad, axis=2):
+    #maps points (unit vecs) to cylinder of known radius along axis (default z/2)
+    #scaled_path = np.empty(path.shape)
+    scales = np.empty(path.shape[0])
+    #define axes perpendicular to the cylinder
+    rad_axes = [0,1,2]
+    rad_axes.remove(axis)
+    
+    #iterate through path and project point
+    for i in range(path.shape[0]):
+        vec = path[i]
+        scale_rad = np.sqrt(np.sum([vec[j]**2 for j in rad_axes]))
+        scale = rad / scale_rad
+        scales[i] = scale
+        #scaled_path[i] = vec * scale
+    return scales#scaled_path
+
+def get_y(angle,x):
+    angle2 = np.pi-angle-np.pi/2
+    return x * np.sin(angle) / np.sin(angle2)
+>>>>>>> scripts/utils_tmp.py
