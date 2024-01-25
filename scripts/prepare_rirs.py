f0264f9bf75393f95b61f5807fb312f3a37dd272
iranroman <iran@ccrma.stanford.edu>
2024-01-24 22:30:46 -0500

    full coverage of utils

--- scripts/prepare_rirs.py
+++ scripts/prepare_rirs.py
@@ -1,30 +1,44 @@
 import os
-from room_scaper import sofa_utils, tau_loading
+import argparse
+import os
+import shutil
+import requests
+import zipfile
+import librosa
+import numpy as np
+import soundfile as sf
+from pathlib import Path
 
-rooms = [
-    "bomb_shelter",
-    "gym",
-    "pb132",
-    "pc226",
-    "sa203",
-    "sc203",
-    "se203",
-    "tb103",
-    "tc352",
-]
+from utils import download_file, extract_zip, combine_multizip
+from spatialscaper import sofa_utils, tau_utils
+
+METU_URL = "https://zenodo.org/record/2635758/files/spargair.zip"
+
+TAU_REMOTES = {
+    "TAU-SRIR_DB.z01": "https://zenodo.org/records/6408611/files/TAU-SRIR_DB.z01?download=1",
+    "TAU-SRIR_DB.z02": "https://zenodo.org/records/6408611/files/TAU-SRIR_DB.z02?download=1",
+    "TAU-SRIR_DB.z03": "https://zenodo.org/records/6408611/files/TAU-SRIR_DB.z03?download=1",
+    "TAU-SRIR_DB.zip": "https://zenodo.org/records/6408611/files/TAU-SRIR_DB.zip?download=1",
+    "TAU-SNoise_DB.z01": "https://zenodo.org/records/6408611/files/TAU-SNoise_DB.z01?download=1",
+    "TAU-SNoise_DB.zip": "https://zenodo.org/records/6408611/files/TAU-SNoise_DB.zip?download=1",
+}
+
+NTAU_ROOMS = 9
+
+TAU_DB_NAME = "TAU-SRIR-DB-SOFA"
 
 
 def create_single_sofa_file(aud_fmt, tau_db_dir, sofa_db_dir, db_name):
-    db_dir = os.path.join(sofa_db_dir, aud_fmt)
+    db_dir = sofa_db_dir
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
-    for room_idx in range(9):
+    for room_idx in range(NTAU_ROOMS):
         # Load flattened (and flipped) rirs/paths from TAU-SRIR database
         rirs, source_pos, mic_pos, room = sofa_utils.load_flat_tau_srir(
             tau_db_dir, room_idx, aud_fmt=aud_fmt
         )
 
-        filepath = os.path.join(db_dir, f"{room}.sofa")
+        filepath = os.path.join(db_dir, f"{room}_{aud_fmt}.sofa")
         comment = f"SOFA conversion of {room} from TAU-SRIR-DB"
 
         print(
@@ -44,84 +58,6 @@ def create_single_sofa_file(aud_fmt, tau_db_dir, sofa_db_dir, db_name):
             comment=comment,
         )
 
-
-def create_per_traj_sofa_file(aud_fmt, tau_db_dir, sofa_db_dir, db_name):
-    db_dir = os.path.join(sofa_db_dir, aud_fmt)
-    if not os.path.exists(db_dir):
-        os.makedirs(db_dir)
-    for room_idx in range(9):
-        room = rooms[room_idx]
-        room_dir = os.path.join(db_dir, room)
-        if not os.path.exists(room_dir):
-            os.makedirs(room_dir)
-        n_traj = tau_loading.check_n_traj(tau_db_dir, room_idx)
-        for traj_idx in range(n_traj):
-            # Load flattened (and flipped) rirs/paths from TAU-SRIR database
-            rirs, source_pos, mic_pos, room = sofa_utils.load_flat_tau_srir(
-                tau_db_dir, room_idx, aud_fmt=aud_fmt, traj=traj_idx
-            )
-
-            filepath = os.path.join(room_dir, f"{room}_t{traj_idx}.sofa")
-            comment = (
-                f"SOFA conversion of {room}, trajectory {traj_idx} from TAU-SRIR-DB"
-            )
-
-            print(
-                f"Creating .sofa file for {aud_fmt}, Room: {room}, trajectory {traj_idx} (Progress: {room_idx + 1}/9, {traj_idx}/{n_traj})"
-            )
-
-            # Create .sofa files with flattened rirs/paths + metadata
-            sofa_utils.create_srir_sofa(
-                filepath,
-                rirs,
-                source_pos,
-                mic_pos,
-                db_name=db_name,
-                room_name=room,
-                listener_name=aud_fmt,
-                sr=24000,
-                comment=comment,
-            )
-
-
-# Call the function for both 'foa' and 'mic'
-tau_db_dir = "TAU_DB/TAU-SRIR_DB"
-sofa_db_dir = "TAU_DB/TAU_SRIR_DB_SOFA"
-# tau_db_dir = '/scratch/ci411/SRIR_DATASETS/TAU_SRIR_DB/TAU-SRIR_DB'
-# sofa_db_dir = '/scratch/ci411/SRIR_DATASETS/TAU_SRIR_DB_SOFA'
-db_name = "TAU-SRIR-DB-SOFA"
-for aud_fmt in ["foa", "mic"]:
-    print(f"Starting .sofa creation for {aud_fmt} format, per traj.")
-    create_per_traj_sofa_file(aud_fmt, tau_db_dir, sofa_db_dir, db_name)
-    print(f"Finished .sofa creation for {aud_fmt} format, per traj.")
-
-    print(f"Starting .sofa creation for {aud_fmt} format.")
-    create_single_sofa_file(aud_fmt, tau_db_dir, sofa_db_dir, db_name)
-    print(f"Finished .sofa creation for {aud_fmt} format.")
-
-import argparse
-import os
-import shutil
-import requests
-import zipfile
-import librosa
-import numpy as np
-import soundfile as sf
-from spatialscaper.sofa_utils import create_srir_sofa
-from pathlib import Path
-
-from utils import download_file, extract_zip, combine_multizip
-
-METU_URL = "https://zenodo.org/record/2635758/files/spargair.zip"
-
-TAU_REMOTES = {
-    "TAU-SRIR_DB.z01": "https://zenodo.org/records/6408611/files/TAU-SRIR_DB.z01?download=1",
-    "TAU-SRIR_DB.z02": "https://zenodo.org/records/6408611/files/TAU-SRIR_DB.z02?download=1",
-    "TAU-SRIR_DB.z03": "https://zenodo.org/records/6408611/files/TAU-SRIR_DB.z03?download=1",
-    "TAU-SRIR_DB.zip": "https://zenodo.org/records/6408611/files/TAU-SRIR_DB.zip?download=1",
-    "TAU-SNoise_DB.z01": "https://zenodo.org/records/6408611/files/TAU-SNoise_DB.z01?download=1",
-    "TAU-SNoise_DB.zip": "https://zenodo.org/records/6408611/files/TAU-SNoise_DB.zip?download=1",
-}
 
 
 def download_and_extract(url, extract_to):
@@ -162,12 +98,12 @@ def prepare_metu(dataset_path):
             X.append(x)
         IRs.append(np.array(X))
 
-    filepath = Path(dataset_path) / "sofa_RIRs" / "metu_sparg.sofa"
+    filepath = Path(dataset_path) / "sofa_RIRs" / "metu_sparg_em32.sofa"
     rirs = np.array(IRs)
     source_pos = np.array(xyzs)
     mic_pos = np.array([[0, 0, 0]])
 
-    create_srir_sofa(
+    sofa_utils.create_srir_sofa(
         filepath,
         rirs,
         source_pos,
@@ -179,7 +115,7 @@ def prepare_metu(dataset_path):
     )
 
 
-def prepare_tau(dest_path):
+def download_tau(dest_path):
     # Download combine and extract zip files
     for filename, url in TAU_REMOTES.items():
         download_file(url, dest_path / filename)
@@ -188,7 +124,14 @@ def prepare_tau(dest_path):
     combine_multizip(f"{dest_path/'TAU-SNoise_DB.zip'}", f"{dest_path/'single.zip'}")
     extract_zip(dest_path / "single.zip", dest_path)
 
+def prepare_tau(path_raw, path_sofa, formats=['foa','mic']):
     # generate Sofa files
+    tau_db_dir = f"{path_raw/'TAU-SRIR_DB'}"
+    sofa_db_dir = path_sofa
+    for aud_fmt in formats:
+        print(f"Starting .sofa creation for {aud_fmt} format.")
+        create_single_sofa_file(aud_fmt, tau_db_dir, sofa_db_dir, TAU_DB_NAME)
+        print(f"Finished .sofa creation for {aud_fmt} format.")
 
 
 if __name__ == "__main__":
@@ -211,5 +154,6 @@ if __name__ == "__main__":
 
     # TAU
     dest_path = Path(args.path) / "raw_RIRs"
-    prepare_tau(dest_path)
->>>>>>> 9fbab0e (moving to temp file)
+    download_tau(dest_path)
+    dest_path_sofa = Path(args.path) / "sofa_RIRs"
+    prepare_tau(dest_path,dest_path_sofa)
